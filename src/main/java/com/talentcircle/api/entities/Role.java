829b742529d1d032d7fb501cbcbc11b399bf6a01
ridermanriquecueto <ridermc@gmail.com>
2026-04-29 17:43:42 -0300

    feat: agregando modelos User y Role

---
+++ src/main/java/com/talentcircle/api/entities/Role.java
@@ -0,0 +1,5 @@
+package com.talentcircle.api.entities;
+
+public enum Role {
+    TALENT, RECRUITER, ADMIN
+}
---
+++ src/main/java/com/talentcircle/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.talentcircle.api.repositories;
+
+import com.talentcircle.api.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
