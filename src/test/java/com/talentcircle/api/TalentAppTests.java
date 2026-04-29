e6b22b6d1519ed5b767226c7e6a9803f0f5f64fb
ridermanriquecueto <ridermc@gmail.com>
2026-04-29 17:31:57 -0300

    feat: estructura inicial de paquetes y entidad User

---
+++ src/test/java/com/talentcircle/api/TalentAppTests.java
@@ -0,0 +1,13 @@
+package com.talentcircle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
