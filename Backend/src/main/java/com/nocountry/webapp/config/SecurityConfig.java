0c4adfda50fbb7af3349ced1e19bf002d202d7ea
anthonybanion <abanon@fi.uba.ar>
2026-05-18 14:32:04 -0300

    [Backend]=> Agrega mejoras al endpoint

--- Backend/src/main/java/com/nocountry/webapp/config/SecurityConfig.java
+++ Backend/src/main/java/com/nocountry/webapp/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.nocountry.webapp.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -53,6 +54,11 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
+                                // Preflight CORS
+                .requestMatchers(
+                        HttpMethod.OPTIONS,
+                        "/**"
+                ).permitAll()
                 .requestMatchers(
                         "/api/auth/**",
                         "/v3/api-docs/**",
