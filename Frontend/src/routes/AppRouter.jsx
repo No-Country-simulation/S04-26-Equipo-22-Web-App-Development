421f57173c07df10dd44f61ebeb3a86f87af90fd
martin aguilera <martin.aguilera0233@gmail.com>
2026-05-24 21:03:45 -0300

    feat: add job status dashboard

--- Frontend/src/routes/AppRouter.jsx
+++ Frontend/src/routes/AppRouter.jsx
@@ -9,6 +9,7 @@ import HomePage from "../views/HomePage";
 import CommunitiesPage from "../views/CommunitiesPage";
 import { ProtectedRoute } from "./ProtectedRoute";
 import AppLayout from "../components/layout/AppLayout";
+import JobStatusPage from "../views/JobStatusPage";
 
 function AppRouter() {
   return (
@@ -31,8 +32,9 @@ function AppRouter() {
           <Route path="/preview" element={<ChannelPreview />} />
           <Route path="/approval" element={<ApprovalPage />} />
           <Route path="/approval/:id" element={<ApprovalPage />} />
+          <Route path="/job-status" element={<JobStatusPage />} />
         </Route>
-      </Routes>
+      </Routes> 
     </BrowserRouter>
   );
 }
