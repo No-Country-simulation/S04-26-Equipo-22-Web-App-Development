37cafd49b6aa8fd0c481561ba5fd253e64f62de8
Alejandro Camacho <gaalexandro58@gmail.com>
2026-05-27 02:01:09 -0400

    fix: conectar drafts a API real y limpiar CORS duplicado

--- Backend/src/main/java/com/nocountry/webapp/config/SecurityConfig.java
+++ Backend/src/main/java/com/nocountry/webapp/config/SecurityConfig.java
@@ -40,7 +40,6 @@ public class SecurityConfig {
                 // MEJORA: Le inyectamos explícitamente tu configuración de CORS
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
-                .cors(withDefaults())
 
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
